a18ed8558120d1315d1ada01a48034f8cbe922b1
TSUN WAI WONG <sunny2323232@gmail.com>
2025-09-15 22:00:44 +0200

    impl. loading labels

--- experiments/category_experiment.py
+++ experiments/category_experiment.py
@@ -3,7 +3,7 @@ import torch
 from tqdm import tqdm
 import gc # Import the garbage collection module
 
-from src.utils.store_representations import save_repr, save_labels
+from src.utils.store_representations import save_repr, save_labels, load_representations, load_labels
 from src.vllm.fastvlm import FastVLM
 from src.vllm.qwen import QwenVLProbe
 from src.vllm.automodel import AutoModelVLM
@@ -12,15 +12,14 @@ from src.utils.experiment_utils import  get_repr_batch, get_repr_for_layer, trai
 # Added for sample negative objects
 import random
 
+USE_OFFLINE_REPR = True
 
-# Added for loading the dataset for local semantics
-dataset, categories = load_category_ds()
-ds = dataset
-ds = ds.train_test_split(0.1)
-ds_train = ds['train']
-ds_eval = ds['test']
+num_dataset_train = 20000
+num_dataset_eval = 2000
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+dataset, categories = load_category_ds()
 
 model_configs = {
     "exp2_1/Qwen_Qwen2-VL-2B-Instruct": ("Qwen/Qwen2-VL-2B-Instruct", QwenVLProbe),
@@ -50,87 +49,108 @@ for experiment_name, (model_hf_name, model_class) in model_configs.items():
     print(f"Processing {experiment_name}")
     print("----------------------------------------------------")
 
-    print(f"Loading model: {model_hf_name}...")
-    model = model_class(model_name=model_hf_name, device=device)
-    print("Model loaded.")
+    if USE_OFFLINE_REPR:
+        repr_train = load_representations("artifacts/repr", experiment_name, "train")
+        reprs_eval = load_representations("artifacts/repr", experiment_name, "eval")
 
-    repr_train = []
-    labels_train = []
-    prompts_train = []
-    imgs_train = []
+        labels_train = []
+        labels_eval = []
 
-    reprs_eval = []
-    labels_eval = []
-    prompts_eval = []
-    imgs_eval = []
+        for i in tqdm(range(num_dataset_train)):
+            labels_train.append(1)
 
-    # Added to shuffle the dataset of local semantics
-    ds_train_sample = ds_train.shuffle().select(range(20000))
-    ds_eval_sample = ds_eval.shuffle().select(range(2000))
+        for i in tqdm(range(num_dataset_eval)):
+            labels_eval.append(1)
 
-    # Added to calculate the length of sampled dataset of local semantics
-    num_dataset_train = len(ds_train_sample)
-    num_dataset_eval = len(ds_eval_sample)
+    else:
+        ds = dataset
+        ds = ds.train_test_split(0.1)
+        ds_train = ds['train']
+        ds_eval = ds['test']
 
-    print("Preparing train data...")
+        print(f"Loading model: {model_hf_name}...")
+        model = model_class(model_name=model_hf_name, device=device)
+        print("Model loaded.")
 
-    for i in tqdm(range(num_dataset_train)):
-        sampled_categories, label_vector, mask_vector = sample_categories(ds_train_sample[i]['pos_categories'], ds_train_sample[i]['neg_categories'])
-        prompt = load_categories_prompt().format(category=', '.join(sampled_categories))
-        prompts_train.append(prompt)
+        repr_train = []
+        labels_train = []
+        prompts_train = []
+        imgs_train = []
 
-        imgs_train.append(ds_train_sample[i]['url'])
+        reprs_eval = []
+        labels_eval = []
+        prompts_eval = []
+        imgs_eval = []
 
-        labels_train.append((label_vector, mask_vector))
+        # Added to shuffle the dataset of local semantics
+        ds_train_sample = ds_train.shuffle().select(range(20000))
+        ds_eval_sample = ds_eval.shuffle().select(range(2000))
 
-    print("Preparing eval data...")
-    for i in tqdm(range(num_dataset_eval)):
-        sampled_categories, label_vector, mask_vector = sample_categories(ds_eval_sample[i]['pos_categories'], ds_eval_sample[i]['neg_categories'])
-        prompt = load_categories_prompt().format(category=', '.join(sampled_categories))
-        prompts_eval.append(prompt)
+        # Added to calculate the length of sampled dataset of local semantics
+        num_dataset_train = len(ds_train_sample)
+        num_dataset_eval = len(ds_eval_sample)
 
-        imgs_eval.append(ds_eval_sample[i]['url'])
+        print("Preparing train data...")
 
-        labels_eval.append((label_vector, mask_vector))
+        for i in tqdm(range(num_dataset_train)):
+            sampled_categories, label_vector, mask_vector = sample_categories(ds_train_sample[i]['pos_categories'], ds_train_sample[i]['neg_categories'])
+            prompt = load_categories_prompt().format(category=', '.join(sampled_categories))
+            prompts_train.append(prompt)
+
+            imgs_train.append(ds_train_sample[i]['url'])
+
+            labels_train.append((label_vector, mask_vector))
+
+        print("Preparing eval data...")
+        for i in tqdm(range(num_dataset_eval)):
+            sampled_categories, label_vector, mask_vector = sample_categories(ds_eval_sample[i]['pos_categories'], ds_eval_sample[i]['neg_categories'])
+            prompt = load_categories_prompt().format(category=', '.join(sampled_categories))
+            prompts_eval.append(prompt)
+
+            imgs_eval.append(ds_eval_sample[i]['url'])
+
+            labels_eval.append((label_vector, mask_vector))
 
 
-    print("Computing representations for train examples")
-    repr_train_batched, _ = get_repr_batch(model, prompts_train, imgs_train)
+        print("Computing representations for train examples")
+        repr_train_batched, _ = get_repr_batch(model, prompts_train, imgs_train)
 
-    print("Computing representations for eval examples")
-    reprs_eval_batched, _ = get_repr_batch(model, prompts_eval, imgs_eval)
+        print("Computing representations for eval examples")
+        reprs_eval_batched, _ = get_repr_batch(model, prompts_eval, imgs_eval)
 
-    for i in range(len(repr_train_batched)):
-        repr_train.append(repr_train_batched[i])
+        for i in range(len(repr_train_batched)):
+            repr_train.append(repr_train_batched[i])
 
-    for i in range(len(reprs_eval_batched)):
-        reprs_eval.append(reprs_eval_batched[i])
+        for i in range(len(reprs_eval_batched)):
+            reprs_eval.append(reprs_eval_batched[i])
 
-    save_repr(
-        representations=repr_train,
-        base_path="artifacts/repr",
-        experiment_name=experiment_name,
-        split_name="train"
-    )
-    save_repr(
-        representations=reprs_eval,
-        base_path="artifacts/repr",
-        experiment_name=experiment_name,
-        split_name="eval"
-    )
 
-    save_labels(
-        labels=labels_train,
-        base_path="artifacts/labels",
-        experiment_name=experiment_name,
-        split_name="train"
-    )
-    save_labels(
-        labels=labels_eval,
-        base_path="artifacts/labels",
-        experiment_name=experiment_name,
-        split_name="eval"
-    )
+        save_labels(
+            labels=labels_train,
+            base_path="artifacts/labels",
+            experiment_name=experiment_name,
+            split_name="train"
+        )
+        save_labels(
+            labels=labels_eval,
+            base_path="artifacts/labels",
+            experiment_name=experiment_name,
+            split_name="eval"
+        )
+
+        save_repr(
+            representations=repr_train,
+            base_path="artifacts/repr",
+            experiment_name=experiment_name,
+            split_name="train"
+        )
+        save_repr(
+            representations=reprs_eval,
+            base_path="artifacts/repr",
+            experiment_name=experiment_name,
+            split_name="eval"
+        )
+
 
     print("Training probes for each layer")
     for layer in tqdm(range(repr_train[0].shape[0]), desc=f"Training Probes for {experiment_name}"):
